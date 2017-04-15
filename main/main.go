808330df16e9fba851f3ba0b54d822aa73fadd08
trickstar0301 <tomida.ldp@gmail.com>
2017-04-16 00:07:02 +0900

    test

--- main/main.go
+++ main/main.go
@@ -13,10 +13,13 @@ import (
 
 func main() {
 	fmt.Print("*** scraping start ***\n")
-	uri := "https://xxx"
+
+	//get file
+	uri := "https://www.xxx"
 	doc, err := goquery.NewDocument(uri)
 	if err != nil {
 		fmt.Print("url scarapping failed\n")
+		os.Exit(0)
 	}
 
 	urlSlice := make([]string, 0)
@@ -32,12 +35,14 @@ func main() {
 		response, err := http.Get(urlSlice[i])
 		if err != nil {
 			panic(err)
+			os.Exit(0)
 		}
 		defer response.Body.Close()
 
-		file, err := os.Create("../images/" + strconv.Itoa(i) + ".jpg")
+		file, err := os.Create("example_scrapping/img/" + strconv.Itoa(i) + ".jpg")
 		if err != nil {
 			panic(err)
+			os.Exit(0)
 		}
 		defer file.Close()
 
@@ -48,11 +53,12 @@ func main() {
 
 	for i,_:=range(urlSlice){
 		pdf.AddPage()
-		filename := ("../images/" + strconv.Itoa(i) + ".jpg")
+		filename := ("example_scrapping/img/" + strconv.Itoa(i) + ".jpg")
 		pdf.Image(filename, 0, 0, nil)
 		log.Print(filename+"\r\n")
 	}
-	pdf.WritePdf("../output/image.pdf")
+	pdf.WritePdf("example_scrapping/pdf/result.pdf")
 
+	defer fmt.Print("*** scraping end ***\n")
 
 }
